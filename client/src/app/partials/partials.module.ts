816c01fe28d2e8722fe872336607c694ad0b6c34
Wong Pak Wah <pwwong3@users.noreply.github.com>
2022-12-28 23:25:32 -0500

    Fix the cart

--- client/src/app/partials/partials.module.ts
+++ client/src/app/partials/partials.module.ts
@@ -5,12 +5,12 @@ import { BasePageComponent } from "./base-page/base-page.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import {CartSummaryComponent} from '../book-store/cart-summary/cart-summary.component';
-
+import { RouterModule } from "@angular/router";
 
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule,],
+    imports: [BrowserModule, FormsModule, RouterModule],
     declarations: [
                   CartSummaryComponent,
                    BasePageComponent,
